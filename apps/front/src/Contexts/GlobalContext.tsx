84a14b2722b1c4324b41949a70d31be5850f1870
Travi995 <joseantoniotravieso259@gmail.com>
2023-11-10 16:02:02 -0500

    feat: Vinculacion del contexto global a la app

--- apps/front/src/Contexts/GlobalContext.tsx
+++ apps/front/src/Contexts/GlobalContext.tsx
@@ -1,7 +1,9 @@
 import  { FC, createContext } from "react";
 import { typeGlobalContext, typeProviderContext } from "../Types/GlobalContext";
 
-
+/**
+ * GLobalContext  es donde se usa todo lo k esta global en la aplicacion
+ */
 export const GlobalContext = createContext<typeGlobalContext>({
 
 
