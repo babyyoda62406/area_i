15ed24a08792f3824c961a73b15706dd22b6ebe6
Travi995 <joseantoniotravieso259@gmail.com>
2023-11-12 16:05:11 -0500

    chore: estilo y seleccion para los elementos del aside

--- apps/front/src/Components/Sidebar/Sidebar.tsx
+++ apps/front/src/Components/Sidebar/Sidebar.tsx
@@ -1,15 +1,27 @@
 
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import './Sidebar.css'
 import { typeSidebar } from '../../Types/TpHlayout'
+import { OptionsSidebar } from '../../Helpers/OptionsSidebar'
 
 
 const Sidebar:FC<typeSidebar> = ({Show}) => {
     
+    const [seleccionado, setSeleccionado] = useState<number>(0)
+
+    const mostrarElemento = (posicion:number) => {
+        setSeleccionado(posicion)
+
+        
+    }
     
 
     return <div className={`Sidebar ${Show ? 'ShowAside': 'CerrarAside'}`}>
+        {OptionsSidebar.map((element, index) => {
+            const{nombre } = element
 
+            return <span className={`ElementAside ${seleccionado===index?'ElementActive':''}`} onClick={()=>{mostrarElemento(index)}} key={index}>{ nombre}</span>
+        })}
 
     </div>
 }
