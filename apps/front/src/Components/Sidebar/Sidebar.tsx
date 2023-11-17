2a8e9c20fd44b2becb832e7a62d50a307ee0c1f2
Travi995 <joseantoniotravieso259@gmail.com>
2023-11-17 13:59:31 -0500

    fix: navBar

--- apps/front/src/Components/Sidebar/Sidebar.tsx
+++ apps/front/src/Components/Sidebar/Sidebar.tsx
@@ -7,40 +7,48 @@ import { Collapse, CollapseProps } from 'antd'
 import { itemASide1, itemASide2, itemASide3 } from '../../Helpers/OptionsSidebar'
 
 
-const Sidebar:FC<typeSidebar> = ({Show}) => {
-    
-    const ElementsStyle = {
-        // marginBottom: '100px'
-    }   
+const Sidebar: FC<typeSidebar> = ({ Show }) => {
 
-    const ElementSidebar:CollapseProps['items'] = [
-    
-        {
-            key: '1',
-            label: itemASide1.name,
-            children: <p>{itemASide1.children}</p>,
-            style:ElementsStyle,
-          },
-          {
-            key: '2',
-            label: itemASide2.name,
-            children: <p>{itemASide2.children}</p>,
-          },
-          {
-            key: '3',
-            label: itemASide3.name,
-            children: <p>{itemASide3.children}</p>,
-          },
-        
-    ]
+  const ElementsStyle = {
+    marginBottom: '',
+    backgroundColor: '#bec8d1',
 
-    
 
-    return <div className={`Sidebar ${Show ? 'ShowAside': 'CerrarAside'}`}>
-        <Collapse accordion items={ElementSidebar} defaultActiveKey={['1']}>
-        </Collapse>    
+  }
 
-    </div>
+  const ElementSidebar: CollapseProps['items'] = [
+
+    {
+      key: '1',
+      className: 'ElementCollapse',
+      label: itemASide1.name,
+      children: <p className='FuncionCollapse'>{itemASide1.children}</p>,
+      style: ElementsStyle,
+    },
+    {
+      key: '2',
+      className: 'ElementCollapse',
+      label: itemASide2.name,
+      children: <p>{itemASide2.children}</p>,
+      style: ElementsStyle,
+    },
+    {
+      key: '3',
+      className: 'ElementCollapse',
+      label: itemASide3.name,
+      children: <p>{itemASide3.children}</p>,
+      style: ElementsStyle,
+    },
+
+  ]
+
+
+
+  return <div className={`Sidebar ${Show ? 'ShowAside' : 'CerrarAside'}`}>
+    <Collapse accordion expandIconPosition='end' size='large' items={ElementSidebar} defaultActiveKey={['1']}>
+    </Collapse>
+
+  </div>
 }
 
 export default Sidebar
