1665af232ba43dbdfc5ed97b3606c06cbb0c90e1
Deivis <deivis@dtm.debiab.com>
2023-11-19 17:52:41 -0500

    feat: Path/ tarifa/:id

--- apps/back/src/modules/tarifa/tarifa.controller.ts
+++ apps/back/src/modules/tarifa/tarifa.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post} from '@nestjs/common';
 import { CrearTarfiaDTO } from './dto/crearTarifa.dto';
 import { TarifaService } from './tarifa.service';
+import { EditarTarfiaDTO } from './dto/editarTarifa.dto';
 
 
 @Controller('tarifa')
@@ -34,6 +35,11 @@ export class TarifaController {
     async deleteTarifa(@Param('id',ParseIntPipe) id:number ){
         const tempTarifa = await  this.svTarifa.eliminarTarifa(id)
         return {message: 'Tarifa eliminada' , id: tempTarifa.id}
+    }
+
+    @Patch(':id')
+    async setTarfia(@Param('id', ParseIntPipe) id:number ,@Body() tarifa:EditarTarfiaDTO){
+        return await this.svTarifa.editarTarifa(id , tarifa) ; 
     }
 
 }
