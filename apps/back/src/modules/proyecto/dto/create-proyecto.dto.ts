221d8d0ef4ec45e379916e9bf3bceef92882b20c
Deivis <deivis@dtm.debiab.com>
2024-04-08 10:47:09 -0400

    fix: POST/Proyectos / NotEmpty

--- apps/back/src/modules/proyecto/dto/create-proyecto.dto.ts
+++ apps/back/src/modules/proyecto/dto/create-proyecto.dto.ts
@@ -1,16 +1,25 @@
-import { IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class CreateProyectoDto {
+    @IsNotEmpty({
+        message:"El nombre del proyecto es obligatorio"
+    })
     @IsString({
         message: "El nombre del proyecto debe ser un string"
     })
     nombre: string
 
+    @IsNotEmpty({
+        message:"El identificador del proyecto es obligatorio"
+    })
     @IsString({
-        message: "El indicador del proyecto debe ser un string"
+        message: "El identificador  del proyecto debe ser un string"
     })
     identificador: string
 
+    @IsNotEmpty({
+        message:"El id de la organización es obligatorio"
+    })
     @IsNumber({}, { message: "El id de la organización debe ser un número" })
     idOrganizacion: number
 }
